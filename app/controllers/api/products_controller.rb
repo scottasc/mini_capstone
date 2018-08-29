2f1e2445d932bb027a81e907028cbc4a9490144d
“scottasc” <scottschulfer@gmail.com>
2018-08-28 19:15:23 -0500

    adding views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,13 @@
 class Api::ProductsController < ApplicationController
+
+  def one_product_action
+    @product = Product.last
+    render 'one_product.json.jbuilder'
+  end
+
+  def all_products_action
+    @products = Product.all
+    render 'all_products.json.jbuilder'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get "/one_product_url" => 'products#one_product_action'
+    get "/all_products_url" => 'products#all_products_action'
+  end
 end
