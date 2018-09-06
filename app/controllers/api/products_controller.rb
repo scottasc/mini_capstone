20e6503a15109c55f7caebcf5e48521343742144
“scottasc” <scottschulfer@gmail.com>
2018-09-05 21:28:14 -0500

    making index dynamic

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,15 +2,13 @@ class Api::ProductsController < ApplicationController
 
   def index
     @products = Product.all
-
+    sort_attribute = params[:sort]
+    sort_order = params[:sort_order]
     search_term = params[:search]
 
     if search_term
       @products = @products.where("description iLIKE ?", "%#{search_term}%")
     end
-
-    sort_attribute = params[:sort]
-    sort_order = params[:sort_order]
 
     if sort_attribute && sort_order
       @products = @products.order(sort_attribute => sort_order)
